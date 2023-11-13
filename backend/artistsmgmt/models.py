7f21bc3eeef70dd32e133cbbda2cd50b54cf652b
warrenshiv <warrenshiv@gmail.com>
2023-11-13 20:56:38 +0300

    Implement user authentication and registration endpoints with Django Rest Framework

--- backend/artistsmgmt/models.py
+++ backend/artistsmgmt/models.py
@@ -1,3 +1,9 @@
+# artistsmgmt/models.py
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
