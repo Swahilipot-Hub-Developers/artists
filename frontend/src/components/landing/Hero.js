b79b546042f85968d6f040027d70284dadab50d7
Kiokosvm <sammysticalfrisk@gmail.com>
2023-11-14 14:32:36 +0300

    Add Footer

--- frontend/src/components/landing/Hero.js
+++ frontend/src/components/landing/Hero.js
@@ -6,9 +6,13 @@ const Hero = () => {
     return(
         <section id='heroproj'>
             <div className='heroproj-container' data-aos="fade-up" data-aos-delay="150" >
-                <h1>"Elevating Talent, Empowering Artists: Your Journey Starts Here."
+                <h1>"Discover,Showcase, Connect, Thrive."
                 </h1>
-                <h2>Discover, Connect, Thrive.We are The Ultimate Hub for Artists and Creatives.
+                <h2>
+                "Exploring the boundless realms of creativity, our artists craft visions that transcend
+                 boundaries. From the brushstroke to the melody, every creation is a testament to the endless
+                 tapestry of human expression. Join us on a journey where art knows no limits, and imagination 
+                 knows no bounds."
                 </h2>
                 <div className='d-flex'>
                     <Link href='#portfolio' className='btn-get-started scrollto'>
--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -1,10 +1,80 @@
-const Footer = () => {
-    const year = new Date().getFullYear()
-    return (
-        <footer className='text-center text-capitalize'>
-            Artists &copy; {year}
-        </footer>
-    )
-}
+import React from 'react';
 
-export default Footer
+const Footer = () => {
+  const footerNavs = [
+    {
+      href: 'javascript:void()',
+      name: 'About',
+    },
+    {
+      href: 'javascript:void()',
+      name: 'Blog',
+    },
+    {
+      href: 'javascript:void()',
+      name: '',
+    },
+    {
+      href: 'javascript:void()',
+      name: 'Team',
+    },
+    {
+      href: 'javascript:void()',
+      name: 'Careers',
+    },
+    {
+      href: 'javascript:void()',
+      name: 'Support',
+    },
+  ];
+
+  return (
+    <footer className="text-gray-500 bg-white px-4 py-3 max-w-screen-xl mx-auto md:px-8 text-center">
+      <div className="container">
+        <img src="/logo/logo.png" className="w-50 mx-auto mb-4 max-w-full" alt="Swahilipot Hub Logo" />
+        <p className="lead">
+          Elevating Our artists through Streamlined management and amplifying creativity with our
+          artist-centric platform.
+        </p>
+      </div>
+      <ul className="list-unstyled d-flex flex-wrap justify-content-center mt-4">
+        {footerNavs.map((item, idx) => (
+          <li key={idx} className="mr-3 mb-2">
+            <a href={item.href} className="text-decoration-none hover:text-gray-800">
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="container mt-4">
+        <div className="mb-4">
+          &copy; 2023 Swahilipot Hub Artists. All rights reserved.
+        </div>
+        <ul className="list-inline mb-0 d-flex justify-content-center">
+          <li className="list-inline-item">
+            <a href="javascript:void()">
+              <img src="/socials/facebook.svg" alt="Facebook" className="w-6 h-6 text-blue-400" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="javascript:void()">
+              <img src="/socials/instagram.svg" alt="Instagram" className="w-6 h-6 text-blue-400" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="javascript:void()">
+              <img src="/socials/twitterx.svg" alt="Twitter(X)" className="w-6 h-6 text-blue-400" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="javascript:void()">
+              <img src="/socials/youtube.svg" alt="Youtube" className="w-6 h-6 text-blue-400" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
