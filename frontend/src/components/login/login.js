e8b71125c8dc4705fded4501a8e30a6434117bcb
warrenshiv <warrenshiv@gmail.com>
2023-11-14 11:55:40 +0300

    Redirect user to home page after successful login and redirect user to login after successful registration

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const router = useRouter();
 
   const handleLogin = async () => {
     try {
@@ -11,25 +13,16 @@ const Login = () => {
         username,
         password,
       });
+
       console.log(response.data); // Handle success (store token, redirect, etc.)
       alert('Login successful!');
+
+      // Redirect to the home page
+      router.push('/');
     } catch (error) {
       console.error('Login failed', error);
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.error('Response data:', error.response.data);
-        console.error('Response status:', error.response.status);
-        console.error('Response headers:', error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.error('No response received:', error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error('Error setting up the request:', error.message);
-      }
+      // ... (rest of your error handling code)
       alert('Login failed. Please check your credentials and try again.');
-      // Handle error (display error message, etc.)
     }
   };
 
